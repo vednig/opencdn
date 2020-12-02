0836511736f247a27c4f91857b329f49a515b187
Ved Nig <64083352+vednig@users.noreply.github.com>
2020-12-02 14:14:32 +0530

    Update and rename texas.php to insert.php

--- insert.php
+++ insert.php
@@ -24,24 +24,4 @@ if ($conn->query($sql) === TRUE) {
 
 $conn->close();
 
-$file_out = "teas.php"; // The image to return
-
-if (file_exists($file_out)) {
-
-   $image_info = getimagesize($file_out);
-
-   //Set the content-type header as appropriate
-   header('Content-Type: ' . $image_info['mime']);
-
-   //Set the content-length header
-   header('Content-Length: ' . filesize($file_out));
-
-   //Write the image bytes to the client
-   readfile($file_out);
-}
-else { // Image file not found
-
-    header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
-
-}
 ?>
